76e3a3b89b6ff26dd74d421d072313fd204dff54
acetim <lilevii1978@gmail.com>
2026-04-27 03:17:22 +0300

    added double fault handling + stack switching

--- src/cpu_interrupts/idt.rs
+++ src/cpu_interrupts/idt.rs
@@ -1,12 +1,14 @@
 use lazy_static::lazy_static;
 use crate::DEBUG;
 use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+use crate::cpu_interrupts::gdt;
 use crate::dbg;
 
 lazy_static! {
     static ref IDT:InterruptDescriptorTable={
         let mut idt = InterruptDescriptorTable::new();
         idt.breakpoint.set_handler_fn(handler_breakpoint);
+        unsafe{idt.double_fault.set_handler_fn(handler_double_fault).set_stack_index(gdt::DOUBLE_FAULT_IST_INDEX)};
         idt
     };
 }
@@ -17,5 +19,8 @@ pub fn idt_init(){
 
 extern "x86-interrupt" fn handler_breakpoint( stack_frame:InterruptStackFrame){
     dbg!("EXCEPTION BREAKPOINT\n {:#?}",stack_frame);
-    
+    //todo handle
+}
+extern "x86-interrupt" fn handler_double_fault(stack_frame:InterruptStackFrame,error_code:u64)->!{
+    panic!("double fault!\n {:#?}\n {}",stack_frame,error_code);
 }
--- src/main.rs
+++ src/main.rs
@@ -4,37 +4,31 @@
 #![feature(abi_x86_interrupt)]
 #![test_runner(crate::testing::test_runner)]
 #![reexport_test_harness_main = "test_main"]
-use core::panic::PanicInfo;
 mod macros;
 mod serial;
 mod tests;
 mod cpu_interrupts;
 mod vga;
 mod testing;
+mod panic;
+
 pub const DEBUG:bool = true;
 
 #[unsafe(no_mangle)]
 pub extern "C" fn _start()->!{
     print_logo();
     init();
-
     #[cfg(test)]
     test_main();
-
     println!("succsessful!");
     loop{}
 }
 
 fn init(){
     cpu_interrupts::idt::idt_init();
+    cpu_interrupts::gdt::gdt_init();
 }
 
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info:&PanicInfo)->!{
-    println!("{}",info);
-    loop{}
-}
 fn print_logo(){
     println!(r#"
                             WELCOME         TO
@@ -43,7 +37,7 @@ fn print_logo(){
                                  9          9 9             9 /
                                  |          \ | 9           |/ 9
          /@@@@@@   /@@@@@@     /\|-----.    /\|/   /\--.  /\|// /#\_  /\.
-        /@@__  @@ /@ __@@@\    |@@@@@@@@\   |@@@|  |@@@|  |@@@| |@@@| @@@\
+        /@@__  @@ /@ __@@@     |@@@@@@@@\   |@@@|  |@@@|  |@@@| |@@@| @@@\
        | @@  \ @@| @@  \__/     \@@@@@@@|    \@@/    @@/   \@@/ \@@/  \@@/
        | @@  | @@|  @@@@@@             ||      ||     @     /|  .''   //
        | @@  | @@ \____  @@            ||      ||    //     || //   ,//

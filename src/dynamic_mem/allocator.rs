3899333db2bc74f60a6cb1502ed7f6094892ed8b
acetim <lilevii1978@gmail.com>
2026-06-06 21:03:11 +0300

    finished small_alloc

--- src/dynamic_mem/allocator.rs
+++ src/dynamic_mem/allocator.rs
@@ -1,6 +1,7 @@
 
 use alloc::alloc::{GlobalAlloc,Layout};
 use core::mem;
+use core::ops::DerefMut;
 use x86_64::structures::paging::{FrameAllocator, Mapper, Page, PageTableFlags, Size4KiB};
 use x86_64::structures::paging::mapper::MapToError;
 use x86_64::VirtAddr;
@@ -8,6 +9,9 @@ use crate::{dbg, paging};
 use crate::utils::locker::Locker;
 use core::ptr::write;
 use paging::frame_allocator::FRAME_ALLOC;
+use paging::setup::KERNEL_PAGE_TABLE;
+use crate::paging::frame_allocator::get_frame_allocator;
+
 #[global_allocator]
 static ALLOCATOR:Locker<KHeapAllocator> = Locker::new(KHeapAllocator::new());
 
@@ -39,7 +43,7 @@ pub const INIT_HEAP_SIZE: usize = NUM_CACHES * 0x1000;
 pub const MIN_ALLOC_VAL:usize = 16;
 //runs before KHeapAllocator::init()
 pub fn kernel_heap_init(
-    mapper_mutex:&mut impl Mapper<Size4KiB>,
+    mapper:&mut impl Mapper<Size4KiB>,
     frame_allocator:&mut impl FrameAllocator<Size4KiB>
 )->Result<(),MapToError<Size4KiB>>{
     let page_range = {
@@ -106,6 +110,12 @@ impl KHeapAllocator{
         Some(head)
     }
     fn small_alloc(&mut self,layout: Layout)->*mut u8{
+        /*
+        responsible to allocate small sized chunks
+        (up to 1kb)
+        returns a raw pointer to that chunk
+        expands the cache when freelist is full
+         */
         //TODO verify min size!!!!!
         let size = layout.size();
         let cache_idx = (64-(size>>4).leading_zeros()) as usize;//strictly 64 bit !
@@ -118,31 +128,53 @@ impl KHeapAllocator{
                 return chunk as *mut u8;
             },
             None=>{
-                //todo expand and try again
+                match self.expand_cache(cache_idx) {
+                    Err(e)=>{panic!("{:?}",e)} //page table error
+                    _=>{}
+                }
+                let new_chunk = self.cache[cache_idx].expect("how DID we get here???");
+                unsafe {
+                    self.cache[cache_idx] = (*new_chunk).next;
+                }
+                return new_chunk as *mut u8;
             }
 
         }
-        0 as *mut u8
     }
-    fn expand_cache(&mut self,cache_idx:usize){
-        let heap_end_addr=HEAP_START+INIT_HEAP_SIZE+(self.expanded_pages_count)*0x1000;
-        let new_page:Page<Size4KiB> = Page::containing_address(VirtAddr::new(heap_end_addr as u64));
+    fn expand_cache(&mut self,cache_idx:usize)
+    -> Result<(),MapToError<Size4KiB>>{
+        /*
+        allocates a new 4kb phys frame for the kheap
+        returns the start address of that new page
+         */
+        let new_page_addr =HEAP_START+INIT_HEAP_SIZE+(self.expanded_pages_count)*0x1000;//addr of new page
+        let new_page:Page<Size4KiB> = Page::containing_address(VirtAddr::new(new_page_addr as u64));
+
         //todo maybe handle this better vv
-        let frame = FRAME_ALLOC.wait()
+        let new_frame = FRAME_ALLOC.wait()
             .expect("error while trying to acquire frame allocator")
             .lock()
             .allocate_frame()
             .expect("physical frame allocation failed: no more memory available!!");
 
         let flags = PageTableFlags::WRITABLE | PageTableFlags::PRESENT;
-        let mapper =
+        let mut mapper =KERNEL_PAGE_TABLE
+            .wait()
+            .expect("kernel page table has not been initialized")
+            .lock();
         unsafe{
-            (/*todo call active level 4 table and use as mapper*/).map_to(new_page,
-                          frame,
-                          flags,
-                          FRAME_ALLOC.wait().expect("error while trying to acquire frame allocator").lock()
+            (*mapper).map_to(
+                new_page,
+                new_frame,
+                flags,
+                get_frame_allocator().lock().deref_mut()
             )?.flush();
         };
+        self.expanded_pages_count+=1;
+        let slot_size = 16<<cache_idx;
+        self.cache[cache_idx]=Self::split_page_to_slots(slot_size,new_page_addr);
+        Ok(())
+
     }
 }
 

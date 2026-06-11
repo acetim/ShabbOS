1f97b496af0a02816cef4cfb37cc994264e903f2
acetim <lilevii1978@gmail.com>
2026-06-11 05:54:28 +0300

    work on vpa fallback alloc

--- src/dynamic_mem/heap_vpage_allocator.rs
+++ src/dynamic_mem/heap_vpage_allocator.rs
@@ -23,48 +23,82 @@ impl VirtualPageAllocator{
 
     }
     fn add_free_region(&mut self,start_new:usize,pages_new:usize) {
-        let layout = Layout::new::<VpaNode>();
-
-
+        /*
+        this function takes a new addr and a number of pages
+        it expands/adds nodes to the freelist
+         */
         let mut current_node = self.freelist_head;
         let mut prev_node = None;
+        //go to new node position
         while(current_node!=None){
-            let start_current;
+            let current_ref;
             unsafe{
-                start_current=(*current_node
-                    .expect("how did we get here"))
-                    .start;
+                current_ref=&*current_node
+                    .expect("how did we get here");
             }
-            if(start_current<start_new){
+            if(current_ref.start<start_new){
                 break;
             }
             prev_node = current_node;
-            unsafe{current_node=(*current_node.unwrap()).next};
+            current_node=current_ref.next;
         }
-        //atp current_node is before where start should be inserted
-
-        let vpa_node_ptr;
-
-        if(prev_node==None&&current_node==None){//no free nodes, set head as new node
-            unsafe{
-                vpa_node_ptr = ALLOCATOR.alloc(layout) as *mut VpaNode;
-                write(vpa_node_ptr,VpaNode{start:start_new,pages:pages_new,next:None})
+        //no free nodes, set head as new node
+        if(prev_node==None&&current_node==None){
+            self.freelist_head = Self::create_vpa_node(
+                start_new,
+                pages_new,
+                None
+            );
+            return
+        }
+        //add/merge node to start
+        if(prev_node==None){
+            let current_ref = unsafe{
+                &mut (*current_node.unwrap())
+            };
+            if(start_new+(pages_new*0x1000)==current_ref.start){//merge
+                current_ref.start=current_ref.start-(pages_new*0x1000);
+                return;
             }
-            self.freelist_head = Some(vpa_node_ptr);
+            self.freelist_head = Self::create_vpa_node(
+                start_new,
+                pages_new,
+                self.freelist_head
+            );
             return
         }
-        if(prev_node==None){//add to start
-            //todo add to start and expand
-            return
-        }
+        //add/merge node to end
         if(current_node==None){
-            //todo add to end and check merge
+            let prev = unsafe{
+                &mut (*prev_node.unwrap())
+            };
+            if(prev.start+(prev.pages*0x1000)==start_new){//merge
+                prev.pages+=pages_new;
+                return
+            }
+            prev.next = Self::create_vpa_node(
+                start_new,
+                pages_new,
+                None
+            );
             return
         }
         //todo add to middle and check merge
-
-
-
+    }
+    fn create_vpa_node(start:usize,pages:usize,next:Option<*mut VpaNode>)->Option<*mut VpaNode>{
+        let layout = Layout::new::<VpaNode>();
+        let vpa_node_ptr;
+        unsafe{
+            vpa_node_ptr = ALLOCATOR.alloc(layout) as *mut VpaNode;
+            write(vpa_node_ptr,
+                  VpaNode {
+                      start,
+                      pages,
+                      next
+                  }
+            )
+        }
+        Some(vpa_node_ptr)
     }
 }
 
